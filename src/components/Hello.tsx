2081868257ca7106dde30128e73d19b9f6c77f03
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2018-10-20 22:51:50 -0700

    Replaced with functional component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,44 +5,23 @@ export interface IProps {
   enthusiasmLevel?: number;
 }
 
-interface IState {
-  currentEnthusiasm: number;
-}
-
-class Hello extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {currentEnthusiasm: props.enthusiasmLevel || 1};
+function Hello({ name, enthusiasmLevel = 1 }: IProps) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
   }
 
-  public render() {
-    const {name} = this.props;
-
-    if (this.state.currentEnthusiasm <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
-    }
-
-    return (
-      <div className="hello">
-        <div className="greeting">
-          Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
-        </div>
-        <button onClick={this.onDecrement}>-</button>
-        <button onClick={this.onIncrement}>+</button>
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
       </div>
-    );
-  }
-
-  private onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
-
-  private onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
-
-  private updateEnthusiasm(currentEnthusiasm: number) {
-    this.setState({currentEnthusiasm});
-  }
+    </div>
+  );
 }
 
 export default Hello;
+
+// helpers
 
 function getExclamationMarks(numChars: number) {
   return Array(numChars + 1).join('!');
